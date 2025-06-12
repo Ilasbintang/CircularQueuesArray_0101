f7e2277d34a13e4b43a0bf24c494e51c98d99813
Ilasbintang <ilasbintang727@gmail.com>
2025-06-12 17:40:14 +0700

    commit 2

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -19,6 +19,14 @@ public:
     {
         int num;
         cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
     }
         
 }
