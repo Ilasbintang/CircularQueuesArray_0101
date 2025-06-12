5f0fbf6c9d5c8510b0d1d08350e750fea1d9e1f9
Ilasbintang <ilasbintang727@gmail.com>
2025-06-12 18:34:43 +0700

    commit 7

--- circularQueuesArray.cpp
+++ circularQueuesArray.cpp
@@ -94,6 +94,15 @@ public:
                 cout << queue_array[FRONT_position] << "  ";
                 FRONT_position++;
             }
+
+            FRONT_position = 0;
+
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
 
